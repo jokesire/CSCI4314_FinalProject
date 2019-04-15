f5fdef64f40c6d4c8e36080fc9c7957d09f5b1c8
demc9402 <demc9402@colorado.edu>
2019-04-14 19:29:04 -0600

    fasta

---
+++ GeneBank.py
@@ -0,0 +1,9 @@
+from Bio import SeqIO
+for seq_record in SeqIO.parse("sequence.fasta", "fasta"):
+    print(seq_record.id)
+    print(repr(seq_record.seq))
+    print(len(seq_record))
+
+sequence_dict = SeqIO.index("sequence.fasta", "fasta")
+print(sequence_dict)
+print(list(sequence_dict.values()))
